51b02f8da64524cf128f51f3e1e68b36eacc080f
Jeril Sebastian <seb988@gmail.com>
2017-10-10 12:48:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+<p align="center">
+  <a href="https://github.com/Raathigesh/Propel/blob/master/LICENSE">
+    <img src="https://img.shields.io/npm/l/express.svg?maxAge=2592000&style=flat-square"
+         alt="License">
+  </a>
+  <a href="https://www.npmjs.com/package/propel">
+    <img src="https://img.shields.io/npm/v/propel.svg?style=flat-square"
+         alt="NPM Version">
+  </a>
+</p>
+
+## Why?
+Certain starter kits I use again and again. But it's a hasle to keep them bookmarked, to find them when required and then clone to a directory.
+
+**Kickstart** will remember them and clone to the desired directory for you with a single command. Though kickstart is all about remembering a git repo and cloning it to a directory, use it for anything you like.
+
+## Installation
+```
+$ npm install kickstart -g
+```
+
+
+Clone a starter kit to the **current working directory**
+```
+kick
+```
+
+
+Add a new starter kit
+```
+kick -a
+```
+
+Remove a start kit
+```
+kick -r
+```
+
+## License
+MIT © 
---
+++ src/kickstart.js
@@ -0,0 +1,143 @@
+'use strict';
+
+const fs = require('fs-extra');
+const program = require('commander');
+const inquirer = require('inquirer');
+const gitclone = require('gitclone');
+const path = require('path');
+const userHome = require('user-home');
+const chalk = require('chalk');
+const ora = require('ora');
+const log = console.log;
+
+let repoList = null;
+createStore();
+
+program
+    .version('1.0.0')
+    .option('-a, --add', 'Add a repository')
+    .option('-r, --remove', 'Remove a repository')
+    .parse(process.argv);
+
+
+if (program.add) addRepo();
+else if (program.remove) removeRepo();
+else {
+    if (repoList === null || repoList.repos.length === 0) {
+    	log(chalk.cyan("No starter kits available. Use the -a option to add a new one"));
+    } else {
+    	cloneRepo();
+    }
+}
+
+
+// Create a JSON file to store the list of 
+// repositories in the user's home directory
+
+function createStore() {
+    const kickstartDir = path.join(userHome, '.kickstart');
+    const kickstartFile = path.join(kickstartDir, 'repoList.json');   
+    fs.ensureFileSync(kickstartFile);
+    
+    repoList = fs.readJsonSync(kickstartFile, { throws: false });
+    if (repoList === null) {
+        repoList = {
+            repos: []
+        };
+        fs.writeFileSync(kickstartFile, JSON.stringify(repoList));
+    }
+}
+
+// Read from the console and 
+// add a repository to the JSON file
+
+async function addRepo() {
+    let answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'repo_name',
+        message: 'Name of the starter kit?'
+    }, {
+        type: 'input',
+        name: 'repo_url',
+        message: 'Git url of the starter kit?'
+    }]);
+
+    if (getRepoName(answers.repo_name) !== undefined) {
+        log(chalk.red('Name already exists. Try a differnt name?'));
+    } else {
+        repoList.repos.push({
+            name: answers.repo_name,
+            url: answers.repo_url
+        });
+        fs.writeFileSync(kickstartFile, JSON.stringify(repoList));
+    }
+}
+
+
+// Read from the console and 
+// remove a repository from the JSON file
+
+async function removeRepo() {
+    var repo_choices_to_remove = [];
+    repoList.repos.forEach(function (item) {
+        repo_choices_to_remove.push({
+            name: item.name
+        });
+    });
+
+    let answers = await inquirer.prompt([{
+        type: 'checkbox',
+        name: 'repos_to_remove',
+        message: 'Select the starter kits to remove.',
+        choices: repo_choices_to_remove
+    }]);
+
+    removeRepoByName(answers.repos_to_remove);
+    fs.writeFileSync(kickstartFile, JSON.stringify(repoList));
+}
+
+
+// Selec the repository from the console and clone it
+
+async function cloneRepo() {
+    var repo_choices = [];
+    repoList.repos.forEach(function (item) {
+        repo_choices.push(item.name);
+    });
+
+    let answers = await inquirer.prompt([{
+        type: 'list',
+        name: 'repo_to_clone',
+        message: 'Choose a starter kit to clone.',
+        choices: repo_choices
+    }]);
+
+    const gitUrl = getRepoName(answers.repo_to_clone).url;
+    const spinner = new ora({ text: 'Cloning ' + answers.repo_to_clone }).start();
+
+    gitclone(gitUrl, (err) => {
+        spinner.stop();        
+        if (err) {
+            log(chalk.red('There was an error while cloning the repository.'));
+        }
+        else {
+            log(chalk.green("  Successfully cloned"));
+        }
+    });
+}
+
+
+// Check if the repository is already added to the JSON file.
+// If yes, return the entry, else return 'undefined'
+
+function getRepoName(repoName) {
+    return repoList.repos.find(_repo => _repo.name == repoName);
+}
+
+
+// Remove a repository from the JSON file
+
+function removeRepoByName(repoName) {
+    let index = repoList.repos.findIndex(_repo => _repo.name == repoName);
+    repoList.repos.splice(index, 1);
+}
